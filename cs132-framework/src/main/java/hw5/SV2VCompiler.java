16eb7a5e86300935e816c77eae935b032f363f80
paul-talma <109061871+paul-talma@users.noreply.github.com>
2026-06-05 21:08:25 -0700

    hw5 passes

--- cs132-framework/src/main/java/hw5/SV2VCompiler.java
+++ cs132-framework/src/main/java/hw5/SV2VCompiler.java
@@ -1,13 +1,14 @@
 package hw5;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import IR.token.Identifier;
 // import IR.syntaxtree.*;
 import sparrowv.*;
 
@@ -16,92 +17,68 @@ public class SV2VCompiler extends sparrowv.visitor.DepthFirst {
     List<String> instructions;
     String currentFunctionName;
     Map<String, Integer> offsets;
-    Set<String> calleeSavedRegisters = new HashSet<>(Arrays.asList(
-            "s1",
-            "s2",
-            "s3",
-            "s4",
-            "s5",
-            "s6",
-            "s7",
-            "s8",
-            "s9",
-            "s10",
-            "s11"));
-    Set<String> usedCalleeSavedRegs;
+    int labelCounter = 0;
 
     public void visit(Program n) {
-        // globals
-        program.add(".equiv @print_int, 1\n");
-        program.add(".equiv @print_string, 4\n");
-        program.add(".equiv @print_char, 11\n");
-        program.add(".equiv @exit, 10\n\n");
-
-        program.add(".text\n");
-        program.add("    jal Main\n");
-        program.add("    li a0, @exit\n");
-        program.add("    ecall\n\n");
+        String entryFunctionName = getEntryFunction(n.funDecls);
+        addPrelude(entryFunctionName);
 
         for (FunctionDecl fd : n.funDecls) {
             fd.accept(this);
             program.addAll(instructions);
         }
+
+        addPostlude();
+    }
+
+    String mangle(String name) {
+        return "_" + name;
     }
 
     public void visit(FunctionDecl n) {
         instructions = new ArrayList<String>();
-        currentFunctionName = n.functionName.toString();
+        currentFunctionName = mangle(n.functionName.toString());
         instructions.add(currentFunctionName + ":\n");
 
-        usedCalleeSavedRegs = new HashSet<String>();
-        offsets = getOffsets(n.block);
-        int numAllocations = offsets.size() + usedCalleeSavedRegs.size();
-        int allocationSize = (numAllocations + 2) * 4;
+        List<Identifier> params = n.formalParameters;
+        List<String> locals = getLocals(n.block, params);
+        offsets = getOffsets(params, locals);
 
-        // assign stack slots for callee-saved regs after the identifier slots
-        Map<String, Integer> calleeSavedOffsets = new HashMap<>();
-        int csOffset = 12 + offsets.size() * 4;
-        for (String reg : usedCalleeSavedRegs) {
-            calleeSavedOffsets.put(reg, csOffset);
-            csOffset += 4;
-        }
+        int numAllocations = locals.size() + 2; // locals + ra + fp
+        int allocationSize = numAllocations * 4;
 
         // prologue
         instructions.add("    sw fp, -8(sp)\n");
-        instructions.add("    lw fp, 0(sp)\n");
+        instructions.add("    mv fp, sp\n");
         instructions.add(String.format("    li t0, %d\n", allocationSize));
         instructions.add("    sub sp, sp, t0\n");
         instructions.add("    sw ra, -4(fp)\n");
 
-        // save callee-saved registers
-        for (Map.Entry<String, Integer> e : calleeSavedOffsets.entrySet()) {
-            instructions.add("    sw " + e.getKey() + ", " + e.getValue() + "(fp)\n");
-        }
-
-        String returnVar = n.block.return_id.toString();
         n.block.accept(this);
-        // TODO: need to handle return id?
 
-        // epilogue
-        // TODO: load return val?
-        // restore callee-saved registers
-        for (Map.Entry<String, Integer> e : calleeSavedOffsets.entrySet()) {
-            instructions.add("    lw " + e.getKey() + ", " + e.getValue() + "(fp)\n");
-        }
+        // epilogue — load return val
+        String returnVar = n.block.return_id.toString();
         Integer returnOffset = offsets.get(returnVar);
         if (returnOffset == null)
             throw new RuntimeException(
                     "return identifier '" + returnVar + "' has no stack slot in " + currentFunctionName);
+        // instructions.add("////// LOAD RETURN VAL\n");
         instructions.add("    lw a0, " + returnOffset + "(fp)\n");
+
+        // restore return address, frame pointer
+        // instructions.add("////// RESTORE RA, FP\n");
         instructions.add("    lw ra, -4(fp)\n");
         instructions.add("    lw fp, -8(fp)\n");
+        // deallocate stack
+        // instructions.add("////// DEALLOCATE STACK\n");
         instructions.add(String.format("    addi sp, sp, %d\n", allocationSize));
-        instructions.add("    jr ra\n");
+        // return
+        instructions.add("    jr ra\n\n");
     }
 
     public void visit(LabelInstr n) {
         String labelName = n.toString();
-        instructions.add(currentFunctionName + "_" + labelName + ":\n");
+        instructions.add(currentFunctionName + "_" + labelName + "\n");
     }
 
     public void visit(Move_Reg_Integer n) {
@@ -112,7 +89,7 @@ public class SV2VCompiler extends sparrowv.visitor.DepthFirst {
     public void visit(Move_Reg_FuncName n) {
         String lhsName = n.lhs.toString();
         String funcName = n.rhs.toString();
-        instructions.add("    la " + lhsName + ", " + funcName + "\n"); // la r, Fun
+        instructions.add("    la " + lhsName + ", " + mangle(funcName) + "\n"); // la r, Fun
     }
 
     public void visit(Add n) {
@@ -187,77 +164,182 @@ public class SV2VCompiler extends sparrowv.visitor.DepthFirst {
 
     public void visit(Print n) {
         String contentReg = n.content.toString();
-        instructions.add("    mv a1, " + contentReg + "\n");
-        instructions.add("    li a0, @print_int\n");
-        instructions.add("    ecall\n");
-        instructions.add("    li a1, 10\n");
-        instructions.add("    li a0, @print_char\n");
-        instructions.add("    ecall\n");
+        instructions.add("    mv a0, " + contentReg + "\n");
+        instructions.add("    jal print\n");
     }
 
     public void visit(ErrorMessage n) {
         String msg = n.msg;
-        instructions.add("    la a1, " + msg + "\n");
-        instructions.add("    li a0, @print_string\n");
-        instructions.add("    ecall\n");
-        instructions.add("    li a1, 10\n");
-        instructions.add("    li a0, @print_char\n");
-        instructions.add("    ecall\n");
-        instructions.add("    li a0, @exit\n");
-        instructions.add("    ecall");
+        if (msg.contains("null")) {
+            instructions.add("    la a0, msg_nullptr\n");
+        } else if (msg.contains("out")) {
+            instructions.add("    la a0, msg_array_oob\n");
+        }
+        instructions.add("    j error\n");
     }
 
     public void visit(Goto n) {
         String label = n.label.toString();
-        instructions.add("    jal " + currentFunctionName + "_" + label + "\n");
+        instructions.add("    j " + currentFunctionName + "_" + label + "\n");
     }
 
     public void visit(IfGoto n) {
         String cond = n.condition.toString();
         String label = n.label.toString();
-
-        instructions.add("    bnez t0, " + currentFunctionName + "_" + label + "no_long_jump\n");
-        instructions.add("    jal " + currentFunctionName + "_" + label + "\n");
-        instructions.add("    " + currentFunctionName + "_" + label + "no_long_jump:\n");
+        String skip = currentFunctionName + "_ifgoto_skip_" + (labelCounter++);
+        instructions.add("    bne " + cond + ", x0, " + skip + "\n");
+        instructions.add("    j " + currentFunctionName + "_" + label + "\n");
+        instructions.add(skip + ":\n");
     }
 
     public void visit(Call n) {
+        String lhs = n.lhs.toString();
+        String callee = n.callee.toString();
+        List<Identifier> args = n.args;
 
+        // store args
+        // instructions.add("////// SAVING ARGS\n");
+        saveArgs(args);
+        // make the call
+        // instructions.add("////// CALL\n");
+        instructions.add("    jalr ra, " + callee + ", 0\n");
+        // move return val into lhs
+        // instructions.add("////// SAVE RETURN VAL\n");
+        instructions.add("    mv " + lhs + ", a0\n");
+        // clean up stack
+        // instructions.add("////// CLEAN UP STACK\n");
+        cleanArgs(args);
+        // instructions.add("////// EXIT CALL INSTR\n");
     }
 
     /*
      * helpers
      */
 
-    void checkCalleeSaved(String reg) {
-        if (calleeSavedRegisters.contains(reg))
-            usedCalleeSavedRegs.add(reg);
+    String getEntryFunction(List<FunctionDecl> funcs) {
+        return funcs.get(0).functionName.toString();
+    }
+
+    void addPrelude(String entryFunctionName) {
+        program.add(".equiv @sbrk, 9\n");
+        program.add(".equiv @print_string, 4\n");
+        program.add(".equiv @print_char, 11\n");
+        program.add(".equiv @print_int, 1\n");
+        program.add(".equiv @exit, 10\n");
+        program.add(".equiv @exit2, 17\n\n");
+
+        program.add(".text\n");
+        program.add(".globl main\n");
+        program.add("main:\n");
+        program.add(" jal " + mangle(entryFunctionName) + "\n");
+        program.add(" li a0, @exit\n");
+        program.add(" ecall\n\n");
+    }
+
+    void addPostlude() {
+
+        program.add("\n.globl print\n");
+        program.add("print:\n");
+        program.add("    mv a1, a0\n");
+        program.add("    li a0, @print_int\n");
+        program.add("    ecall\n");
+        program.add("    li a1, 10\n");
+        program.add("    li a0, @print_char\n");
+        program.add("    ecall\n");
+        program.add("    jr ra\n\n");
+
+        program.add(".globl error\n");
+        program.add("error:\n");
+        program.add("    mv a1, a0\n");
+        program.add("    li a0, @print_string\n");
+        program.add("    ecall\n");
+        program.add("    li a1, 10\n");
+        program.add("    li a0, @print_char\n");
+        program.add("    ecall\n");
+        program.add("    li a0, @exit\n");
+        program.add("    ecall\n");
+        program.add("abort_17:\n");
+        program.add("    j abort_17\n\n");
+
+        program.add(".globl alloc\n");
+        program.add("alloc:\n");
+        program.add("    mv a1, a0\n");
+        program.add("    li a0, @sbrk\n");
+        program.add("    ecall\n");
+        program.add("    jr ra\n\n");
+        program.add(".data\n\n");
+
+        program.add(".globl msg_nullptr\n");
+        program.add("msg_nullptr:\n");
+        program.add("    .asciiz \"null pointer\"\n");
+        program.add("    .align 2\n\n");
+
+        program.add(".globl msg_array_oob\n");
+        program.add("msg_array_oob:\n");
+        program.add("    .asciiz \"array index out of bounds\"\n");
+        program.add("    .align 2\n");
+    }
+
+    void saveArgs(List<Identifier> args) {
+        // allocate space
+        int nArgs = args.size();
+        instructions.add("    li t6, " + nArgs * 4 + "\n");
+        instructions.add("    sub sp, sp, t6\n");
+
+        // store args
+        int offset = 0;
+        for (Identifier a : args) {
+            String argName = a.toString();
+            Integer frameOffset = offsets.get(argName);
+            if (frameOffset == null) {
+                // identifier never written to; callee reads from a2/a3/... anyway
+                instructions.add("    sw x0, " + offset + "(sp)\n");
+            } else {
+                instructions.add("    lw t6, " + frameOffset + "(fp)\n");
+                instructions.add("    sw t6, " + offset + "(sp)\n");
+            }
+            offset += 4;
+        }
+    }
+
+    void cleanArgs(List<Identifier> args) {
+        int nArgs = args.size();
+        instructions.add("    li t6, " + nArgs * 4 + "\n");
+        instructions.add("    add sp, sp, t6\n");
+    }
+
+    // return local stack-allocated variables (unique, excluding params)
+    List<String> getLocals(Block b, List<Identifier> params) {
+        Set<String> paramNames = new HashSet<>();
+        for (Identifier p : params)
+            paramNames.add(p.toString());
+        LinkedHashSet<String> vars = new LinkedHashSet<>();
+        for (Instruction i : b.instructions) {
+            if (i instanceof Move_Id_Reg) {
+                String lhs = ((Move_Id_Reg) i).lhs.toString();
+                if (!paramNames.contains(lhs))
+                    vars.add(lhs);
+            } else if (i instanceof Move_Reg_Id) {
+                String rhs = ((Move_Reg_Id) i).rhs.toString();
+                if (!paramNames.contains(rhs))
+                    vars.add(rhs);
+            }
+        }
+        return new ArrayList<>(vars);
     }
 
     // offset(var) = x if the value of var is at x(fp)
-    // also records def'ed callee-saved regs
-    Map<String, Integer> getOffsets(Block b) {
-        Set<String> vars = new HashSet<>();
-        for (Instruction i : b.instructions) {
-            if (i instanceof Move_Id_Reg) {
-                vars.add(((Move_Id_Reg) i).lhs.toString());
-            } else if (i instanceof Move_Reg_Id) {
-                vars.add(((Move_Reg_Id) i).rhs.toString());
-                checkCalleeSaved(((Move_Reg_Id) i).lhs.toString());
-            } else if (i instanceof Move_Reg_Integer) {
-                checkCalleeSaved(((Move_Reg_Integer) i).lhs.toString());
-            } else if (i instanceof Move_Reg_Reg) {
-                checkCalleeSaved(((Move_Reg_Reg) i).lhs.toString());
-            } else if (i instanceof Move_Reg_FuncName) {
-                checkCalleeSaved(((Move_Reg_FuncName) i).lhs.toString());
-            }
-        }
-
+    Map<String, Integer> getOffsets(List<Identifier> params, List<String> locals) {
         Map<String, Integer> offsets = new HashMap<>();
         int offset = -12;
-        for (String name : vars) {
-            offsets.put(name, offset);
+        for (String local : locals) {
+            offsets.put(local, offset);
             offset -= 4;
+        }
+        offset = 0;
+        for (Identifier p : params) {
+            offsets.put(p.toString(), offset);
+            offset += 4;
         }
         return offsets;
     }
@@ -265,4 +347,5 @@ public class SV2VCompiler extends sparrowv.visitor.DepthFirst {
     public List<String> getProgram() {
         return program;
     }
+
 }
